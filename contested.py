d0252bdccf5749b18b7486b3210a3facea8577e8
Abhinav <singh.avi99@gmail.com>
2021-06-16 16:13:29 -0500

    view posts

--- contested.py
+++ contested.py
@@ -1,16 +1,15 @@
-from flask import Flask, jsonify, request,render_template
+from flask import Flask, jsonify, request, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
+from key import get_key
 import os
 import requests
 import sqlite3
 
 app = Flask(__name__)
-
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+#this value is not stored on github, change if you want to run your own local version
+SECRET_KEY = get_key()
+app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/')
 def index():
@@ -21,7 +20,26 @@ def index():
 
 @app.route('/super_simple')
 def super_simple():
-    return 'Super simple!'
+    return SECRET_KEY
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    if post is None:
+        return jsonify('try again'), 404
+    return render_template('post.html', post=post)
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    conn.close()
+    return post
 
 if __name__ == '__main__':
     app.run()
