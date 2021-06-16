df1f37203722042c2c256dfc112c2b728638e0f5
Abhinav <singh.avi99@gmail.com>
2021-06-16 15:20:22 -0500

    database creation

--- contested.py
+++ contested.py
@@ -3,14 +3,25 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
 import os
 import requests
-from flask_marshmallow import Marshmallow
+import sqlite3
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
 
 @app.route('/super_simple')
 def super_simple():
     return 'Super simple!'
+
+if __name__ == '__main__':
+    app.run()
